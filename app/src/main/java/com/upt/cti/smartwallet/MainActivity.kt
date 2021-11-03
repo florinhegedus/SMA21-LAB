9a1828888e0d80f62b24fbc40f8b6f500abef1ec
Florin Hegedus <florinxhgds@gmail.com>
2021-11-03 12:47:27 +0200

    created repo

---
+++ app/src/main/java/com/upt/cti/smartwallet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.upt.cti.smartwallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
