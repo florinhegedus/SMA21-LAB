65fd5bfdac906649d453f701d19a8632ca9a5cf2
Florin Hegedus <florinxhgds@gmail.com>
2021-11-03 16:07:34 +0200

    added MonthlyExpenses class

---
+++ app/src/main/java/com/upt/cti/smartwallet/model/MonthlyExpenses.kt
@@ -0,0 +1,7 @@
+package com.upt.cti.smartwallet.model
+
+class MonthlyExpenses(month: String, income: Float, expenses: Float) {
+    public var month: String? = ""
+    private var income: Float? = 0f
+    private var expenses: Float? = 0f
+}
