a37ffbe807292ea3a20bd774f8129388ec29e6b6
Florin Hegedus <florinxhgds@gmail.com>
2021-11-11 11:51:05 +0200

    finished lab6

--- app/src/main/java/com/upt/cti/smartwallet/MainActivity.kt
+++ app/src/main/java/com/upt/cti/smartwallet/MainActivity.kt
@@ -2,10 +2,31 @@ package com.upt.cti.smartwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.upt.cti.smartwallet.model.MonthlyExpenses
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseDatabase.getInstance("https://smart-wallet-6240c-default-rtdb.europe-west1.firebasedatabase.app/").reference
+        db.setValue("12345678")
+
+        val src = findViewById<Button>(R.id.searchButton)
+        src.setOnClickListener{
+            Toast.makeText(this, "Searching", Toast.LENGTH_SHORT).show()
+
+            val srcMonth = findViewById<EditText>(R.id.monthName)
+            var searchMonth = srcMonth.text.toString()
+
+            db.child(searchMonth).setValue(MonthlyExpenses(searchMonth, 1f, 2f))
+        }
+
     }
 }
--- app/src/main/java/com/upt/cti/smartwallet/model/MonthlyExpenses.kt
+++ app/src/main/java/com/upt/cti/smartwallet/model/MonthlyExpenses.kt
@@ -1,7 +1,13 @@
 package com.upt.cti.smartwallet.model
 
-class MonthlyExpenses(month: String, income: Float, expenses: Float) {
-    public var month: String? = ""
-    private var income: Float? = 0f
-    private var expenses: Float? = 0f
+class MonthlyExpenses {
+    var month: String = ""
+    var income: Float = 0f
+    var expenses: Float = 0f
+
+    constructor(month: String, income: Float, expenses: Float){
+        this.month = month
+        this.income = income
+        this.expenses = expenses
+    }
 }
