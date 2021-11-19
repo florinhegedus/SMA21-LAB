f3b349034005ff4080ea9c6b360e23c9fbf1a8da
Florin Hegedus <florinxhgds@gmail.com>
2021-11-19 15:16:15 +0200

    spinner v2

--- app/src/main/java/com/upt/cti/smartwallet/MainActivity.kt
+++ app/src/main/java/com/upt/cti/smartwallet/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.upt.cti.smartwallet
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import com.google.firebase.database.*
 import com.upt.cti.smartwallet.model.MonthService
@@ -32,15 +34,28 @@ class MainActivity : AppCompatActivity() {
 
         val spinner: Spinner = findViewById(R.id.spinner)
         val arraySpinner = getMonthsFromDB()
-        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arraySpinner)
+        val arrayAdapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_spinner_item, arraySpinner)
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner!!.setAdapter(arrayAdapter)
+        spinner.adapter = arrayAdapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+                Toast.makeText(applicationContext, parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
+        }
+
 
     }
 
-    fun getMonthsFromDB(): ArrayList<String>{
+    private fun getMonthsFromDB(): ArrayList<String>{
         val db = FirebaseDatabase.getInstance("https://smart-wallet-6240c-default-rtdb.europe-west1.firebasedatabase.app/").reference
-        val months: ArrayList<String> = ArrayList<String>()
+        var months: ArrayList<String> = ArrayList()
         db.child("calendar").addValueEventListener( object :
             ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
@@ -53,7 +68,12 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
+        //months = arrayListOf("")
         return months
 
+    }
+
+    private fun getMonthsFromDB2(): ArrayList<String>{
+        return arrayListOf("Ana", "are", "mere")
     }
 }
