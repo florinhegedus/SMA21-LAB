00742ffad4d8458a42e649c33f9474b9f0b7ba62
Florin Hegedus <florinxhgds@gmail.com>
2021-11-21 12:09:37 +0200

    implemented working adapter

--- app/src/main/java/com/upt/cti/smartwallet/LauncherActivity.kt
+++ app/src/main/java/com/upt/cti/smartwallet/LauncherActivity.kt
@@ -2,6 +2,12 @@ package com.upt.cti.smartwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.upt.cti.smartwallet.model.FirebaseHelper
 
 class LauncherActivity : AppCompatActivity() {
@@ -9,6 +15,28 @@ class LauncherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
 
-        FirebaseHelper.init()
+        //FirebaseHelper.init()
+        var listView = findViewById<ListView>(R.id.listview)
+
+        val listItems = arrayOf("Item1", "Item2", "Item3")
+
+
+        val db = FirebaseDatabase.getInstance("https://smart-wallet-6240c-default-rtdb.europe-west1.firebasedatabase.app/").reference
+        db.child("smart wallet").addListenerForSingleValueEvent( object :
+            ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var months = ArrayList<String>()
+                for (postSnapshot in snapshot.children) {
+                    val name: String = postSnapshot.getKey().toString()
+                    months.add(name)
+                }
+
+                val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, months)
+                listView.adapter = adapter
+            }
+        })
     }
 }
